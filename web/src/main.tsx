15c3c7f2fff2bc668f9b667e53e5639ef38ff118
luckyking <luckyyuan@gmail.com>
2025-11-15 18:32:25 +0800

    chore(release): baseline on release/baseline-2025-11-15

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import { Toaster } from 'sonner'
 import './index.css'
+import { initAnalytics } from './lib/analytics'
+
+initAnalytics()
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
